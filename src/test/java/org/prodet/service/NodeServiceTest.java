9879858513a96ec5eeebeb554e74d9a0bace1009
janake <janak.endre@gmail.com>
2017-06-12 09:15:51 +0200

    Create NodeServiceTest.java
    remove all unnecesary lines

--- src/test/java/org/prodet/service/NodeServiceTest.java
+++ src/test/java/org/prodet/service/NodeServiceTest.java
@@ -1,18 +1,5 @@
 package org.prodet.service;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
-import org.prodet.repository.domain.Node;
-import org.prodet.repository.repository.NodeRepositoryInterface;
-import org.prodet.service.DAO.NodeDao;
-
-@RunWith(MockitoJUnitRunner.class)
 public class NodeServiceTest {
 	
 }
