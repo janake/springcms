bd48e97de6f7ae77f098b42a07eb6095894cb30b
janake <janak.endre@gmail.com>
2017-06-12 09:14:04 +0200

    Create NodeServiceTest.java
    remove unnecessary lines

--- src/test/java/org/prodet/service/NodeServiceTest.java
+++ src/test/java/org/prodet/service/NodeServiceTest.java
@@ -14,28 +14,5 @@ import org.prodet.service.DAO.NodeDao;
 
 @RunWith(MockitoJUnitRunner.class)
 public class NodeServiceTest {
-
-	@Mock
-	NodeRepositoryInterface nodeRepository;
 	
-	@Mock
-	NodeDao nodeFromController;
-	
-	@Test
-	public void testSave() {
-		
-		Node node = mock(Node.class);
-//		NodeDao nodeFromController = mock(NodeDao.class);
-		when(nodeFromController.getTitle()).thenReturn("Ez az első címem");
-		when(nodeFromController.getBody()).thenReturn("Ez az első body-m");
-		
-		node.setTitle(nodeFromController.getTitle());
-		node.setBody(nodeFromController.getBody());
-		Node expected = nodeRepository.save(node);
-		
-		Node actual = nodeRepository.findOne(expected.getId());
-		
-		Assert.assertEquals(expected.getTitle(), actual.getTitle());		
-	}
-
 }
