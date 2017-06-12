042bba5bc062abe5fc1a55ec0a6048cd790da309
janake <janak.endre@gmail.com>
2017-06-12 09:32:15 +0200

    test változások

--- src/test/java/org/prodet/service/NodeServiceTest.java
+++ src/test/java/org/prodet/service/NodeServiceTest.java
@@ -1,12 +1,5 @@
 package org.prodet.service;
 
-<<<<<<< HEAD
-import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
-
-@RunWith(MockitoJUnitRunner.class)
-=======
->>>>>>> refs/remotes/origin/master
 public class NodeServiceTest {
 	
 }
